1fbfa0db895e756cc0f1992b8fb0530f0ffed09f
Daniel WALSER <daniel.walser@student.htl-rankweil.at>
2025-05-27 08:09:26 +0200

    Hier ist der erste Teil von Heute: Erstellung der Deck-Klasse inkl. Speichern von Decks etc.

---
+++ Vokabeltrainer/Vokabeltrainer/Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vokabeltrainer
+{
+    public class Deck
+    {
+        // Hier wird die Liste initialisiert, welches im Konstruktor somit verwendet wird.
+        private List<Flashcardlist> deck = new List<Flashcardlist>();
+
+        private Deck(List<Flashcardlist> deck)
+        {
+            // Hier wird eine Liste von einer Flashcardliste gespeichert
+            deck = new List<Flashcardlist> ();
+        }
+
+        public void Speichern(string path)
+        {
+            // Hier sollte das Deck gespeichert werden.
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                
+                for (int i = 0; i < deck.Count; i++) // Durchläuft alle Werte aus der Liste
+                {
+                    Flashcard card = new Flashcard();
+                    
+                    sw.WriteLine(card.Serialize());
+                }
+            }
+        }
+    }
+}
--- Vokabeltrainer/Vokabeltrainer/Projekt.md
+++ Vokabeltrainer/Vokabeltrainer/Projekt.md
@@ -28,13 +28,16 @@
     - Erweiterungsmöglichkeiten
 
 !!! info Projekt Zeitplan
-    |Datum|Beschreibung|Status|
-    |---|---|---|
-    |**29.04/30.04**|MUST-Haves und NICE-Haves bestimmen|100%|
-    |**06.05**|UML-Diagramm entwerfen, GUI-Zeichnung erstellen|100%/80%|
-    |**07.05**|Gui Zeichnen|100%|
-    |**13.05**|GUI Zeichnung umsetzen/anwenden|30%|
-    |**14.05**|Gui Zeichnung umsetzen/anwenden|80%|
+    |Datum|Beschreibung|Status|Issue|
+    |---|---|---|---|
+    |**29.04/30.04**|MUST-Haves und NICE-Haves bestimmen|100%||
+    |**06.05**|UML-Diagramm entwerfen, GUI-Zeichnung erstellen|100%/80%||
+    |**07.05**|Gui Zeichnen|100%||
+    |**13.05**|GUI Zeichnung umsetzen/anwenden|30%||
+    |**14.05**|Gui Zeichnung umsetzen/anwenden|90%| edit fehlt|
+    |**16.05**|Speichern und laden umsetzen/verbessern|60%||
+    |**17.05**|Speichern und laden umsetzen/verbessern|90%| doppelter Eintrag von der Hinterseite|
+    |||||
 
 !!! Example Lastenheft
     |Must-Haves|
