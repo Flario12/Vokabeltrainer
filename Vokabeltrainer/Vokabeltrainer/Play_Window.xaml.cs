337a82a23e1e7b1b149fbb15d5c134c7fd896e50
Daniel WALSER <daniel.walser@student.htl-rankweil.at>
2025-06-02 08:45:00 +0200

    Hier ist der erste Teil von Heute: Hier wurde das Play-Window erstellt. Danach kommt noch das Funktionieren dieses Fensters.

---
+++ Vokabeltrainer/Vokabeltrainer/Play_Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vokabeltrainer
+{
+    /// <summary>
+    /// Interaction logic for Play_Window.xaml
+    /// </summary>
+    public partial class Play_Window : Window
+    {
+        public Play_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
